21321351ad656839e5abb4826c3bb6a986527569
Yugenn <yuruseajyan@gmail.com>
2021-09-18 15:51:51 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::get('/', [App\Http\Controllers\ItemController::class, 'index']);
 
 Route::resource('items',
     App\Http\Controllers\ItemController::class);
 
-Route::get('/', [App\Http\Controllers\ItemController::class, 'index']);
