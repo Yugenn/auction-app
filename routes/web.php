fa12157be23f7e1a7ca41b7533a4711b122ac884
Yugenn <yuruseajyan@gmail.com>
2021-09-18 15:55:35 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,5 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::get('/', [App\Http\Controllers\ItemController::class, 'index']);
 
-Route::resource('items',
-    App\Http\Controllers\ItemController::class);
+Route::resource('items', App\Http\Controllers\ItemController::class);
 
